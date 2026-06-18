54c419134a109b5f8d1f4f9a5d6bc39c37ff6520
benoitmondjo <benoitmondjo@gmail.com>
2026-06-18 01:36:28 +0100

    update admin dashboard

--- app/(back_office)/dashboard/(profile)/(all)/page.tsx
+++ app/(back_office)/dashboard/(profile)/(all)/page.tsx
@@ -1,6 +1,6 @@
 "use client"
+import Link from "next/link"
 import { useState } from "react"
-import "@/style/tailwind.css";
 
 const profils = [
   {
@@ -51,61 +51,101 @@ const profils = [
     description: "Dev et architecte...",
     lienPortfolio: "https://www.githubPage.io",
   },
+  
+   
+
 ]
 
-export default function Dashboard() {
-  const [menuOuvert, setMenuOuvert] = useState<number | null>(null)
-  return (
-    <div className="p-6">
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold font-sora">Profils</h1>
-        <button className="bg-primary text-white px-4 py-2 rounded-lg cursor-pointer shadow font-sora hover:text-primary hover:bg-white duration-700">Ajouter un profil +</button>
-      </div>
+function ThHeadTable({label}: {label: string})
+{
+    return (
+        <th className="p-2 text-sm text-gray-900 py-5">{label}</th>
+    )
+}
 
-      <table className="w-full bg-white rounded-lg shadow table-fixed">
-        <thead>
-          <tr className="border-b text-left font-sora ">
-            <th className="p-4 text-[10] max-w-xs truncate">Profil</th>
-            <th className="p-4 text-[10] max-w-xs truncate">Nom</th>
-            <th className="p-4 text-[10] max-w-xs truncate">Prénom</th>
-            <th className="p-4 text-[10] max-w-xs truncate">Spécialité</th>
-            <th className="p-4 text-[10] max-w-xs truncate">Description</th>
-            <th className="p-4 text-[10] max-w-xs truncate">Lien portfolio</th>
-            <th className="p-4 text-[10]">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {profils.map((profil) => (
-            <tr key={profil.id} className="shadow  hover:bg-gray-50 duration-500">
-              <td className="p-4">
+type TypeProfile ={
+    profil: {
+        id: string,
+        nom: string,
+        prenom: string,
+        specialite: string,
+        description: string,
+        lienPortfolio: string
+    }
+}
+function ProfileTr({profil}: {profil: TypeProfile})
+{
+    const [menuOuvert, setMenuOuvert] = useState<number | null>(null)
+
+
+    return (
+        <tr key={profil.id} className="shadow  hover:bg-gray-200 duration-100">
+            <td className="p-2">
                 <div className="w-8 h-8 rounded-full flex justify-center items-center text-white bg-primary font-sora">{profil.id}</div>
-              </td>
-              <td className="p-4 font-sora text-sm max-w-xs truncate">{profil.nom}</td>
-              <td className="p-4 font-sora text-sm max-w-xs truncate">{profil.prenom}</td>
-              <td className="p-4 font-sora text-sm max-w-xs truncate">{profil.specialite}</td>
-              <td className="p-4 font-sora text-sm max-w-xs truncate">{profil.description}</td>
-              <td className="p-4 font-sora text-sm max-w-xs truncate">{profil.lienPortfolio}</td>
-              <td className="p-4 relative">
+            </td>
+            <td className="p-2 text-sm max-w-xs truncate text-gray-800">{profil.nom}</td>
+            <td className="p-2 text-sm max-w-xs truncate text-gray-800">{profil.prenom}</td>
+            <td className="p-2 text-sm max-w-xs truncate text-gray-800">{profil.specialite}</td>
+            <td className="p-2 text-sm max-w-xs truncate text-gray-800">{profil.description}</td>
+            <td className="p-2 text-sm max-w-xs truncate text-gray-800 hover:underline">
+                <Link href={profil.lienPortfolio}>{profil.lienPortfolio}</Link>
+            </td>
+            <td className="p-4 relative">
                 <button className="font-sora cursor-pointer" onClick={() => setMenuOuvert(menuOuvert === profil.id ? null : profil.id)}>...</button>
 
                 {menuOuvert === profil.id && (
-                  <div className="absolute right-0  w-[240] shadow-lg z-10 flex flex-col gap-2 p-10 bg-white/20 backdrop-blur-md border border-white/30 rounded-lg">
+                <div className="absolute right-0  w-[240] shadow-lg z-10 flex flex-col gap-2 p-10 bg-white/20 backdrop-blur-md border border-white/30 rounded-lg">
                     <button className="bg-primary text-white px-4 py-2 rounded-lg flex items-center justify-center gap-2 hover:opacity-90 transition-opacity duration-300 cursor-pointer">
-                      Modifier profil
+                    Modifier profil
                     </button>
                     <button className="bg-primary text-white px-4 py-2 rounded-lg flex items-center justify-center gap-2 hover:opacity-90 transition-opacity duration-300 cursor-pointer">
-                      Supprimer profil
+                    Supprimer profil
                     </button>
                     {/* <button className="bg-primary text-white px-4 py-2 rounded-lg flex items-center justify-center gap-2 hover:opacity-90 transition-opacity duration-300 cursor-pointer">
-                      Consulter profil
+                    Consulter profil
                     </button> */}
-                  </div>
+                </div>
                 )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+            </td>
+        </tr>
+    )
+}
+
+export default function Dashboard() {
+
+    return (
+        <section>
+            <div className="flex justify-between items-center mb-6">
+                <h1 className="text-2xl md:text-3xl  font-bold">Profils</h1>
+                <div className="flex gap-4">
+                    <div className="flex items-center gap-2">
+                        <h3 className="text-xl font-semibold">Total</h3>
+                        <div className="bg-primary text-white px-4 py-2 rounded-lg ">12</div>
+                    </div>
+
+                    <button className="bg-primary text-white px-4 py-2 rounded-lg cursor-pointer">Ajouter un profil +</button>
+                </div>
+            </div>
+
+            <table className="w-full bg-white rounded-lg shadow table-fixed">
+                <thead>
+                    <tr className="border-b border-gray-300 text-left">
+                        <ThHeadTable label="N" />
+                        <ThHeadTable label="Non" />
+                        <ThHeadTable label="Prénom" />
+                        <ThHeadTable label="Spécialité" />
+                        <ThHeadTable label="Description" />
+                        <ThHeadTable label="portfolio" />
+                        <ThHeadTable label="Action" />
+                    </tr>
+                </thead>
+
+                <tbody className="max-h-100 overflow-y-auto">
+                    {profils.map((profil) => (
+                        <ProfileTr key={profil.id} profil={profil} />
+                    ))}
+                </tbody>
+            </table>
+        </section>
+    )
 }
